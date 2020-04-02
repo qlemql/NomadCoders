664dc95a59d35ef2c3886b3ae475baf65d3bde81
penguin <busgamer@naver.com>
2020-04-02 21:31:40 +0900

    #3.9 Getting the Weather part One (Geolocation)
    사용자 위치 저장하기
    
    api key

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "d4e07388c20816f7b6dcd43bc1f34fac";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latituede = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latituede,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo loaction");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadCoords = localStorage.getItem(COORDS);
+  if (loadCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+init();
