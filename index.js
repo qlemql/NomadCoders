1b35db1716a5672e91c2de1300370361ff59e6fa
penguin <busgamer@naver.com>
2020-04-27 23:25:52 +0900

    js-seven
    1. form과 input 생성
    2.js로 html Element생성
    ( document.createElement)
    3.부모안에 자식 넣기(?)
    (appendChild)
    4.input으로 작성한걸 localStorage에 저장, 불러오기

---
+++ index.js
@@ -0,0 +1,58 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoPending = document.querySelector(".js-toDoPending"),
+  toDoFinished = document.querySelector(".js-toDoFinished");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paidntToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const checkBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
+  checkBtn.innerText = "✔";
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(checkBtn);
+  li.id = newId;
+  toDoPending.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paidntToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paidntToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
