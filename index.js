449a4238b4f64f2129be8953288d3d9cac53dbff
penguin <busgamer@naver.com>
2020-04-28 23:25:18 +0900

    js-seven-2
    finished 로컬스토리지 생성
    
    delteFinishedDO 함수 생성
    
    **필터**
    
    transFinDos, transToDo
    이부분에 대해서는 조금 더 알아봐야함.
    
    const data = new Date();
      const newId = data.getTime();
    이 부분 역시 알아봐야함
    
    finished 리스트 생성

--- index.js
+++ index.js
@@ -4,11 +4,51 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoFinished = document.querySelector(".js-toDoFinished");
 
 const TODOS_LS = "toDos";
+const FINISHED_LS = "finishedDo";
 
-const toDos = [];
+let toDos = [];
+let finDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoPending.removeChild(li);
+  const cleanToPending = toDos.filter((toDo) => {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToPending;
+  saveToDos();
+}
+
+function deleteFinishedDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoFinished.removeChild(li);
+  const cleanToFinished = finDos.filter((finDo) => {
+    return finDo.id !== parseInt(li.id);
+  });
+  finDos = cleanToFinished;
+  saveFinDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function saveFinDos() {
+  localStorage.setItem(FINISHED_LS, JSON.stringify(finDos));
+}
+
+function transFinDos(event) {
+  const spanText = event.path[1].childNodes[0].innerHTML;
+  deleteFinishedDo(event);
+  paidntToDo(spanText);
+}
+
+function transToDo(event) {
+  const spanText = event.path[1].childNodes[0].innerHTML;
+  deleteToDo(event);
+  finishedToDo(spanText);
 }
 
 function paidntToDo(text) {
@@ -16,9 +56,12 @@ function paidntToDo(text) {
   const delBtn = document.createElement("button");
   const checkBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newId = toDos.length + 1;
+  const data = new Date();
+  const newId = data.getTime();
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   checkBtn.innerText = "✔";
+  checkBtn.addEventListener("click", transToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
@@ -31,6 +74,31 @@ function paidntToDo(text) {
   };
   toDos.push(toDoObj);
   saveToDos();
+}
+
+function finishedToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const returnBtn = document.createElement("button");
+  const span = document.createElement("span");
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteFinishedDo);
+  returnBtn.innerText = "⏪";
+  returnBtn.addEventListener("click", transFinDos);
+  const data = new Date();
+  const newId = data.getTime();
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.appendChild(returnBtn);
+  li.id = newId;
+  toDoFinished.appendChild(li);
+  const finDoObj = {
+    text: text,
+    id: newId,
+  };
+  finDos.push(finDoObj);
+  saveFinDos();
 }
 
 function handleSubmit(event) {
