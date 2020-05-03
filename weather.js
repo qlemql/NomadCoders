76c2f6f106a37f06bdfb2abc2e7236044fd290d6
penguin <busgamer@naver.com>
2020-05-03 12:37:11 +0900

    날씨및 위치 추가
    .

--- weather.js
+++ weather.js
@@ -1,18 +1,35 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "d4e07388c20816f7b6dcd43bc1f34fac";
 const COORDS = "coords";
+
+function getWeather(lat, log) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (resopnse) {
+      return resopnse.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSucces(position) {
-  const latituede = position.coords.latitude;
+  const latituede = position.coords.latituede;
   const longitude = position.coords.longitude;
   const coordsObj = {
     latituede,
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latituede, longitude);
 }
 
 function handleGeoError() {
@@ -28,7 +45,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    // getWeather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latituede, parsedCoords.longitude);
   }
 }
 
