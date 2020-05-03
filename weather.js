ec01d3abaf0edd4d6a02c0d375480a339cf79b96
penguin <busgamer@naver.com>
2020-05-03 13:16:19 +0900

    ..
    ..

--- weather.js
+++ weather.js
@@ -3,18 +3,18 @@ const weather = document.querySelector(".js-weather");
 const API_KEY = "d4e07388c20816f7b6dcd43bc1f34fac";
 const COORDS = "coords";
 
-function getWeather(lat, log) {
+function getWeather(lat, lng) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
   )
-    .then(function (resopnse) {
-      return resopnse.json();
+    .then(function (response) {
+      return response.json();
     })
     .then(function (json) {
       const temperature = json.main.temp;
       const place = json.name;
       weather.innerText = `${temperature} @ ${place}`;
-    });
+    }); //then은 데이터가 완전히 들어온 다음에 함수를 호출
 }
 
 function saveCoords(coordsObj) {
