efd2dc0302250fa097d2040f347a773db8860f17
qlemql <busgamer@naver.com>
2020-06-11 05:42:42 +0900

    수정
    .

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function handleCM(event) {
 }
 
 function handleSaveClick() {
-  const image = canvas.toDataURL("image/png");
+  const image = canvas.toDataURL();
   const link = document.createElement("a");
   link.href = image;
   link.download = "PaintJS[🎨]";
