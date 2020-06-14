2a1c10fc8106be32a3d920b0170386284e465c17
qlemql <busgamer@naver.com>
2020-06-14 21:12:36 +0900

    서버 만들기
    req, res
    
    GET, POST

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("blablabla");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
