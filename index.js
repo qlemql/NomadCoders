49c359cc45ef5cf2aa6473c05d74c51a6c321e40
qlemql <busgamer@naver.com>
2020-06-14 23:45:10 +0900

    Middlewave
    nodemon --exec babel-node index.js 주소 뒤에 --delay N으로 몇초 딜레이를 걸 수 있다.
    
    express에서의 모든 함수는 middleware가 될 수 있다.
    
    express 서버가 양파같다고 생각하고
    middleware은 중간에 있는거라 생각하면 편하다.
    마지막 함수가 뭔가를 유저에게 반환
    반환할게 없다면 로딩중으로 나옴
    
    개발자가 원하는만큼 middleware을 가질 수 있다.(편함)
    ex) 파일 전송중인걸 가로챌 수 있다.
    ex) 유저가 접속중인걸 확인할 수 있다.등등
    
    항상 위에서 아래로 작동하는걸 기억하자 순서 중요!!!!

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("blablabla");
 
+//middleware "/" 사이와 handleHome 사이에서 발생
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
+//원하는 만큼 middlewave를 넣을 수 있다.
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
