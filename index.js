3d6b42fbf661f784735dc5b2b8ede40063cc66ac
qlemql <busgamer@naver.com>
2020-06-14 23:59:30 +0900

    middleware2
    Morgan
    
    tiny로 로깅 기능을 가지게 된다.
    
    (어떤 접속(GET)인지, 어디로 접속하는지, StatusCode는 무엇인지 알 수 있다.)
    
    combined은
    어떤 종류의 접속인지, 어떤 브라우저인지등등 정보를 얻을 수 있다.
    
    morgan말고 helmet도 있다.
    (보안용)
    
    가끔은 middleware가 연결을 끊을 수 있다.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import "core-js";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+
 const app = express();
 
 const PORT = 4000;
@@ -12,13 +15,22 @@ const handleHome = (req, res) => res.send("Hello from home");
 const handleProfile = (req, res) => res.send("blablabla");
 
 //middleware "/" 사이와 handleHome 사이에서 발생
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
+// const betweenHome = (req, res, next) => {
+//   console.log("Between");
+//   next();
+// };
 
-app.use(betweenHome);
+// app.use(betweenHome);
 //원하는 만큼 middlewave를 넣을 수 있다.
+
+app.use(helmet());
+app.use(morgan("dev"));
+
+// 아래와 같이 연결을 끊을 수 있다.
+// const middleware = (req, res, next) => {
+//   res.send("not happening");
+// };
+// app.get("/", middleware, handleHome); 사이에 middleware넣어주기
 
 app.get("/", handleHome);
 
