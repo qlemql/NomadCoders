71b29056b3191545e618e6a89d14011d9586871f
qlemql <busgamer@naver.com>
2020-06-15 00:06:38 +0900

    middleware3
    cookie parser
    cookie에 유저 정보를 저장
    session을 다루기 위함
    
    body parser
    body로 부터 정보를 얻을 수 있게 해준다.
    body parser에는 정의해야할 옵션이 있다.
    ex)json, text, urlencoded등등
    우리의 서버는 우리가 무엇을 전송하는지 알아야 한다.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "core-js";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser, { urlencoded } from "body-parser";
 
 const app = express();
 
@@ -23,6 +25,9 @@ const handleProfile = (req, res) => res.send("blablabla");
 // app.use(betweenHome);
 //원하는 만큼 middlewave를 넣을 수 있다.
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
