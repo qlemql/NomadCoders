67419cb27dc8acde3ab9a344cd1b5ddc3b6c4b8e
qlemql <busgamer@naver.com>
2020-06-15 20:58:32 +0900

    MVC parttern part Two
    Route 오브젝트 생성

--- routes.js
+++ routes.js
@@ -21,3 +21,20 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 // 위 3개(videos + video_detail + edit_video를 합칠 경우 => videos/1/edit
 const DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO,
+};
