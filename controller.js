f54ba3d895ac7596a0adad3d1ff462a21080c5de
qlemql <busgamer@naver.com>
2020-06-19 18:01:00 +0900

    Day2
    Those URLs should be divided in FIVE different routers.
    Each route needs to have a controller function imported from a "controllers" file.
    NO Anonymous functions allowed. Every route should have a controller.

---
+++ controller.js
@@ -0,0 +1,12 @@
+export const home = (req, res) => res.send("Home");
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
+export const account = (req, res) => res.send("Account");
+export const edit = (req, res) => res.send("Edit");
+export const courses = (req, res) => res.send("Courses");
+export const coursesNew = (req, res) => res.send("Courses New");
+export const coursesMine = (req, res) => res.send("Courses Mine");
+export const documentation = (req, res) => res.send("Documentation");
+export const buy = (req, res) => res.send("Buy");
+export const refund = (req, res) => res.send("Refund");
+export const remove = (req, res) => res.send("Remove");
