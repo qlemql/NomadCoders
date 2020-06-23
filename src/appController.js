9bb69c3b745fb0dc462e44bd1009a55609a40ade
qlemql <busgamer@naver.com>
2020-06-23 18:38:52 +0900

    Day3
    4개의 라우터 생성 / /login /photos /profile
    
    각 라우터는 PUG 템플릿을 렌더링해야함
    
    모든 라우터는 컨트롤러를 가져야함
    
    템플릿은 views 폴더에 넣을 것
    
    모든 템플릿은 레이아웃에서 확장되어야함
    
    레이아웃은 페이지의 head부분과 footer 부분을 포함해야할 것
    
    body에서 각 페이지에는 페이지 제목이 있는 h1이 있어야 함
    
    head에는 각 페이지에 제목과 웹사이트 제목이 있는 title이 있어야함
    
    페이지 제목과 웹사이트는 템플릿에 작성XXXXXX
    
    페이지 제목은 컨트롤러에서 가져와야 함
    
    웹사이트의 제목은 컨트롤러에서 가져오지말고 locals에서 가져올 것
    
    하나의 라우터 파일과 하나의 컨트롤러 파일이 있어야할 것
    
    미들웨어는 자체 파일이 있어야 할 것

---
+++ src/appController.js
@@ -0,0 +1,5 @@
+export const home = (req, res) => res.render("page", { pageTitle: "Home" });
+export const login = (req, res) => res.render("page", { pageTitle: "Login" });
+export const photos = (req, res) => res.render("page", { pageTitle: "Photos" });
+export const profile = (req, res) =>
+  res.render("page", { pageTitle: "Profile" });
