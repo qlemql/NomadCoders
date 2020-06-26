a178c55e029c64b08045271a455873fb22e246b7
qlemql <busgamer@naver.com>
2020-06-26 22:32:52 +0900

    Home Controller part Two

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ export const videos = [
     title: "Video awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 2345,
       name: "Hyun",
@@ -16,7 +17,8 @@ export const videos = [
     title: "Video peng",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 2345,
       name: "Hyun",
@@ -28,7 +30,8 @@ export const videos = [
     title: "Video guin",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 2345,
       name: "Hyun",
@@ -40,7 +43,8 @@ export const videos = [
     title: "Video penguin",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 2345,
       name: "Hyun",
