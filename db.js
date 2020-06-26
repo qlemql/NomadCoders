6662de78a52e14f42bbc26a1853caa440679bebd
qlemql <busgamer@naver.com>
2020-06-26 22:39:13 +0900

    영상 수정

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ export const videos = [
     description: "This is something I love",
     views: 24,
     videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      "https://media.istockphoto.com/videos/king-penguins-on-salisbury-plains-video-id1126705429",
     creator: {
       id: 2345,
       name: "Hyun",
@@ -18,7 +18,7 @@ export const videos = [
     description: "This is something I love",
     views: 24,
     videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      "https://media.istockphoto.com/videos/herd-of-penguin-swimming-from-right-hand-to-left-hand-side-video-id1035991598",
     creator: {
       id: 2345,
       name: "Hyun",
@@ -31,7 +31,7 @@ export const videos = [
     description: "This is something I love",
     views: 24,
     videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      "https://media.istockphoto.com/videos/adelie-penguins-in-antarctic-video-id922077084",
     creator: {
       id: 2345,
       name: "Hyun",
