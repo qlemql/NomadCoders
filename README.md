14163fa8172b105e4a6adf9ad74c88fc1fac9936
qlemql <busgamer@naver.com>
2020-06-26 19:12:55 +0900

    Home Controller

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Cloning Youtube with Vanilla and NodeJS
 - [x] Join
 - [x] Login
 - [x] Search
-- [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
+- [] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
 - [ ] Video Detail
-- [ ] Edit Video
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 32345,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2345,
+      name: "Hyun",
+      email: "Hyun@kim.com",
+    },
+  },
+  {
+    id: 436778,
+    title: "Video peng",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2345,
+      name: "Hyun",
+      email: "Hyun@kim.com",
+    },
+  },
+  {
+    id: 6578679,
+    title: "Video guin",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2345,
+      name: "Hyun",
+      email: "Hyun@kim.com",
+    },
+  },
+  {
+    id: 32345,
+    title: "Video penguin",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 2345,
+      name: "Hyun",
+      email: "Hyun@kim.com",
+    },
+  },
+];
